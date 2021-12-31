142f30dd6dab040f1b63a70470238915813c0378
grasshopper7 <grass.hopper.moc@gmail.com>
2021-12-31 14:47:59 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Spark Extent Report for REST API testing with REST Assured using TestNG
+
+Refer to this [article](https://ghchirp.tech/3791/) for more details.
---
+++ src/test/java/endpoints/CityAirDataIT.java
@@ -0,0 +1,50 @@
+package endpoints;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.testng.SkipException;
+import org.testng.annotations.Test;
+
+import io.qameta.allure.restassured.AllureRestAssured;
+import tech.grasshopper.allure.Author;
+import tech.grasshopper.allure.Authors;
+import tech.grasshopper.allure.Categories;
+import tech.grasshopper.allure.Category;
+import tech.grasshopper.allure.Device;
+import tech.grasshopper.allure.Devices;
+
+@Categories({ @Category("Cities") })
+@Authors({ @Author("John") })
+@Devices({ @Device("Computer") })
+public class CityAirDataIT {
+
+	private static final String token = "1a5d8f027e344fa191b88e966bdb5d4aa4853d4a";
+
+	@Test
+	@Categories({ @Category("Europe") })
+	@Devices({ @Device("Mac"), @Device("Windows") })
+	public void getLondonAirData() {
+		given().param("token", token).filter(new AllureRestAssured()).cookie("Cookie 1", "cookie one")
+				.cookie("Cookie 2", "cookie two").header("Header 1", "header 1").when()
+				.get("https://api.waqi.info/feed/london/").then().statusCode(equalTo(200));
+	}
+
+	@Test
+	@Categories({ @Category("Europe") })
+	@Authors({ @Author("Jane") })
+	@Devices({ @Device("Mac") })
+	public void getMunichAirData() {
+		given().param("token", token).filter(new AllureRestAssured()).cookie("Cookie 1", "cookie one")
+				.cookie("Cookie 2", "cookie two").when().get("https://api.waqi.info/feed/munich/").then()
+				.statusCode(equalTo(201));
+	}
+
+	@Test
+	@Categories({ @Category("Fictional") })
+	@Authors({ @Author("Lewis") })
+	@Devices({ @Device("Kindle") })
+	public void getNarniaAirData() {
+		throw new SkipException("Skipped");
+	}
+}
